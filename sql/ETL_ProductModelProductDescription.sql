3dd3c5472b041bc244b7f000fa8cbd851f71f0dc
pei.luo <peiluo@melco-resorts.com>
2022-09-06 11:57:02 +0800

    [luopei] update sql

---
+++ sql/ETL_ProductModelProductDescription.sql
@@ -0,0 +1,66 @@
+-- workflow=ProductModelProductDescription
+--  period=1440
+--  loadType=incremental
+--  logDrivenType=timewindow
+
+-- stepId=1
+-- sourceConfig
+--  dataSourceType=temp
+-- targetConfig
+--  dataSourceType=variables
+-- checkPoint=false
+-- dateRangeInterval=0
+select from_unixtime(unix_timestamp('${DATA_RANGE_START}', 'yyyy-MM-dd HH:mm:ss'), 'yyyy')                                as `YEAR`,
+       from_unixtime(unix_timestamp('${DATA_RANGE_START}', 'yyyy-MM-dd HH:mm:ss'), 'MM')                                  as `MONTH`,
+       from_unixtime(unix_timestamp('${DATA_RANGE_START}', 'yyyy-MM-dd HH:mm:ss'), 'dd')                                  as `DAY`;
+
+
+-- step=2
+-- source=mysql
+--  dbName=raw
+--  tableName=ProductModelProductDescription
+-- target=hive
+--  dbName=ods
+--  tableName=ProductModelProductDescription
+-- writeMode=overwrite
+-- skipFollowStepWhenEmpty=true
+select ProductModelID	AS ProductModelID,
+       ProductDescriptionID	AS ProductDescriptionID,
+       Culture	AS Culture,
+       rowguid	AS rowguid,
+       ModifiedDate	AS ModifiedDate,
+       '${YEAR}' AS "year",
+       '${MONTH}' AS "month",
+       '${DAY}' AS "day"
+from raw.ProductModelProductDescription
+where ModifiedDate >= '${DATA_RANGE_START}' and ModifiedDate < '${DATA_RANGE_END}'
+
+
+-- step=3
+-- source=hive
+--  dbName=ods
+--  tableName=ProductModelProductDescription
+-- target=hive
+--  dbName=dw
+--  tableName=ProductModelProductDescription
+-- writeMode=overwrite
+select ProductModelID	AS ProductModelID,
+       ProductDescriptionID	AS ProductDescriptionID,
+       Culture	AS Culture,
+       rowguid	AS rowguid,
+       ModifiedDate	AS ModifiedDate
+       from ProductModelProductDescription_his
+where `year` =  '${YEAR}' and `month` = '${MONTH}' and `day` = '${DAY}'
+union all
+select ProductModelID	AS ProductModelID,
+       ProductDescriptionID	AS ProductDescriptionID,
+       Culture	AS Culture,
+       rowguid	AS rowguid,
+       ModifiedDate	AS ModifiedDate
+       from dw.ProductModelProductDescription as a
+left join ods.ProductModelProductDescription as b on
+       a.ProductModelID = b.ProductModelID and
+       a. ProductDescriptionID = b. ProductDescriptionID and
+       a. Culture = b. Culture 
+       and b.`year` =  '${YEAR}' and b.`month` = '${MONTH}' and b.`day` = '${DAY}'
+where b.ProductModelID  is null
