1b047b2c40882ef213a23b0b8589f3b10b5b10cf
pei.luo <peiluo@melco-resorts.com>
2022-09-06 18:54:38 +0800

    [luopei] update deploy.sh and shell script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 poetry run etl-cli create --resource resources  --overwrite
 scp -r ./sql  sz03@master:/home/sz03/
+scp -r ./shell  sz03@master:/home/sz03/
 ssh sz03@master "source ~/.bash_profile && hdfs dfs -put -f  ./sql/* /etl/sql && exit"
 
