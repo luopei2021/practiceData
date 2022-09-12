5cac47f3967f1c42af677be9657d9f434e35480a
pei.luo <peiluo@melco-resorts.com>
2022-09-13 00:07:57 +0800

    [peiluo]fix ddl

--- shell/update_report.sh
+++ shell/update_report.sh
@@ -1,1 +1,1 @@
-spark-submit --master yarn --class com.github.sharpdata.sharpetl.spark.Entrypoint hdfs://master:9000/etl/spark.jar batch-job --names=report_1,report_2,report_3 --period=1440 --default-start-time="2000-01-01 00:00:00" --property=hdfs://master:9000/etl/etl.properties
+spark-submit --master yarn --class com.github.sharpdata.sharpetl.spark.Entrypoint hdfs://master:9000/etl/spark.jar batch-job --names=report_1,report_2,report_3 --period=1440 --default-start-time="2022-09-12 00:00:00" --property=hdfs://master:9000/etl/etl.properties
